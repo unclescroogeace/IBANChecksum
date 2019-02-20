4af0ce7b5526fd6190148304c731a96196347b07
uclescroogeace <venombg.dev@gmail.com>
2019-02-20 12:38:49 +0200

    first commit

---
+++ IBANChecksum/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace IBANChecksum
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkButton = new System.Windows.Forms.Button();
+            this.validationResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IBAN:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(13, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(251, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // checkButton
+            // 
+            this.checkButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkButton.Location = new System.Drawing.Point(277, 29);
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(75, 24);
+            this.checkButton.TabIndex = 2;
+            this.checkButton.Text = "Validate";
+            this.checkButton.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.checkButton.UseVisualStyleBackColor = true;
+            this.checkButton.Click += new System.EventHandler(this.checkButton_Click);
+            // 
+            // validationResult
+            // 
+            this.validationResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.validationResult.Location = new System.Drawing.Point(15, 56);
+            this.validationResult.Name = "validationResult";
+            this.validationResult.Size = new System.Drawing.Size(249, 60);
+            this.validationResult.TabIndex = 5;
+            this.validationResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 121);
+            this.Controls.Add(this.validationResult);
+            this.Controls.Add(this.checkButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "IBAN Validator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button checkButton;
+        private System.Windows.Forms.Label validationResult;
+    }
+}
+
---
+++ IBANChecksum/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IBANChecksum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            validationResult.Text = "";
+            textBox1.Text = "";
+        }
+
+        private void checkButton_Click(object sender, EventArgs e)
+        {
+            String clientIBAN = textBox1.Text;
+
+            if (checkIBAN(clientIBAN))
+            {
+                validationResult.ForeColor = ColorTranslator.FromHtml("#33cc33");
+                validationResult.Text = "VALID";
+            }
+            else
+            {
+                
+                validationResult.ForeColor = ColorTranslator.FromHtml("#ff0000");
+                validationResult.Text = "NOT VALID";
+            }
+        }
+
+        private bool checkIBAN(string clientIBAN)
+        {
+            clientIBAN = clientIBAN.Replace(" ", String.Empty);
+            if (clientIBAN.Length == 22)
+            {
+                if (Regex.IsMatch(clientIBAN, @"^[A-Z0-9]+$") && (clientIBAN[0] == 'B' && clientIBAN[1] == 'G'))
+                {
+                    String iban = clientIBAN.Substring(4, clientIBAN.Length - 4) + clientIBAN.Substring(0, 4);
+                    int shiftNum = 55;
+                    int value;
+                    StringBuilder sb = new StringBuilder();
+
+                    foreach(char c in iban)
+                    {
+                        if (Char.IsLetter(c))
+                        {
+                            value = c - shiftNum;
+                        }
+                        else
+                        {
+                            value = int.Parse(c.ToString());
+                        }
+                        sb.Append(value);
+                    }
+
+                    string checksumString = sb.ToString();
+                    int checksum = int.Parse(checksumString.Substring(0, 1));
+                    for (int i = 1; i < checksumString.Length; i++)
+                    {
+                        value = int.Parse(checksumString.Substring(i, 1));
+                        checksum *= 10;
+                        checksum += value;
+                        checksum %= 97;
+                    }
+
+                    return checksum == 1;
+                }
+                else
+                {
+                    return false;
+                }
+            }else
+            {
+                return false;
+            }
+        }
+    }
+}
